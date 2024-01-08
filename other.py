172ec9f36a2ac74d80777266297313b82655a47f
ShangguanKL <wkwproassist@gmail.com>
2024-01-08 18:06:06 +0900

    del

--- other.py
+++
@@ -1,9 +0,0 @@
-import time
-
-def print_message_for_15_seconds():
-    for i in range(15):
-        print(f"Message {i+1}", flush=True)
-        time.sleep(1)
-
-if __name__=="__main__":
-    print_message_for_15_seconds()
